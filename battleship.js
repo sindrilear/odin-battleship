e1c4905bbf9c5ef8d59cb474c867540f4df0d35c
sindrilear <125163217+sindrilear@users.noreply.github.com>
2025-09-24 11:41:56 -0400

    Create Gameboard class and place ship function

--- battleship.js
+++ battleship.js
@@ -28,7 +28,64 @@ class Ship {
 }
 
 const carrier = new Ship(5)
+const battleship = new Ship(4)
+const cruiser = new Ship(3)
+const submarine = new Ship(3)
+const patrolboat = new Ship(2)
 
-console.log(carrier.checkLength());
+class Gameboard {
+    constructor(rows, columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.board = [];
+    }
+
+    createBoard() {
+        const rows = this.rows;
+        const columns = this.columns;
+        const grid = [];
+
+        for (let i = 0; i < rows; i++) {
+            grid[i] = [];
+            for (let j = 0; j < columns; j++) {
+                grid[i][j] = 0;
+            }
+        }
+        this.board = grid;
+    }
+
+    displayBoard() {
+        for (let i = 0; i < this.rows; i++) {
+            console.log(this.board[i])
+            for (let j = 0; j < this.columns; j++) {
+            }
+        }
+    }
+
+    placeShip(length, x, y, orientation) {
+        if (orientation == "horizontal") {
+            if (length  + x > this.columns) {
+                return console.log("Invalid placement")
+            }
+            for (let i = 0; i < length; i++) {
+                this.board[y][x + i] = "S"
+            }
+        } else if (orientation == "vertical") {
+            if (length  + y > this.rows) {
+                return console.log("Invalid placement")
+            }
+            for (let i = 0; i < length; i++) {
+                this.board[y + i][x] = "S"
+            }
+        }
+    }
+}
+
+    const player = new Gameboard(10, 10);
+
+    player.createBoard();
+    player.placeShip(5, 5, 1, "vertical");
+    player.displayBoard();
+
 
 module.exports = { carrier }
